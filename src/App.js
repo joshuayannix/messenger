4b6ed58bfb3d1cce218b40b6e1f36c30bc849cdf
joshuayannix <joshuayannix@gmail.com>
2020-10-03 17:18:09 -0400

    messages showing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([ 'hey is this available', 'no it is not', 'positive messages only']);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = event => {
+    // all logic to send a message
+    setMessages([...messages, input]);
+    setInput('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello josh</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send message</button>
+      
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
+      
     </div>
   );
 }
