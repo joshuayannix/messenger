9b3c914a0c5f67459ecaabd49c5a55d2ac8d13db
joshuayannix <joshuayannix@gmail.com>
2020-10-03 23:33:53 -0400

    messages pulling and putting to firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Message from './Message';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([
-    {username: 'Josh', text: 'hey whats up'},
-    {username: 'George', text: 'nothing much, you?'}
+
    ]);
   const [username, setUsername] = useState('');
 
@@ -15,12 +16,24 @@ function App() {
   //useEffect = code that runs based on a condition. Runs when the component loads
 
   useEffect(() => {
+    db.collection('messages').onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))
+    })
+  }, []);
+
+  useEffect(() => {
     setUsername(prompt('Please enter your name'));
-  }, [] )
+  }, [] );
+
   const sendMessage = event => {
-    // all logic to send a message
     event.preventDefault();
-    setMessages([...messages, {username: username, text: input}]);
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
+
+    // setMessages([...messages, {username: username, message: input}]);
     setInput('');
   };
 
